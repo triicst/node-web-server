6e96eb39d5a1b1069a64cd062077c330c7f24c28
triicst <tri.pham.hpc@gmail.com>
2018-08-28 17:47:46 +0700

    Updated to port heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.use((req,res,next) =>{
@@ -47,4 +48,6 @@ app.get('/bad',(req,res) =>{
        errorMessage:'Page not found'
    }); 
 });
-app.listen(3000);
+app.listen(port, () =>{
+    console.log(`Starting webserver with port ${port}`);
+});
